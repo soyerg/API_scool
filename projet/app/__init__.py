64247239410cb5075418711811296210d76ac0ff
gabriel soyer <150786176+soyerg@users.noreply.github.com>
2025-02-21 16:09:17 +0100

    Add files via upload

---
+++ projet/app/__init__.py
@@ -0,0 +1,1 @@
+# Ce fichier peut être vide, il sert simplement à indiquer que le répertoire est un module Python.
---
+++ projet/app/crud.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+import models as models
+import schemas as schemas
+import logging
+
+logger = logging.getLogger(__name__)
+
+# Supprimer les fonctions CRUD pour Item
